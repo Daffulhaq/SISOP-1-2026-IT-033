e79802793c0e1429a82145a97a35143edde3627a
Daffulhaq <5027251033@student.its.ac.id>
2026-03-24 23:41:43 +0700

    Soal 1 kode doang

---
+++ soal_1/KANJ.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+file=$1
+option=$2
+
+case $option in
+a)
+count_passenger=$(awk -F',' 'NR>1 {count++} END {print count}' $file)
+echo "Jumlah seluruh penumpang KANJ adalah ${count_passenger} orang"
+;;
+
+b)
+carriage=$(awk -F',' 'NR>1 {print $4}' $file | sort -u | wc -l)
+echo "Jumlah gerbong penumpang KANJ adalah ${carriage} gerbong"
+;;
+
+c)
+IFS="|" read oldest age <<< $(awk -F ',' 'NR>1{
+if($2>max){max=$2; nama=$1}
+}
+END {print nama "|" max}' $file)
+
+echo "${oldest} adalah penumpang kereta tertua dengan usia ${age} tahun"
+;;
+
+d)
+average_age=$(awk -F',' 'NR>1 {total+=$2; count++}
+END {print int(total/count)}' $file)
+
+echo "Rata-rata usia penumpang adalah ${average_age} tahun"
+;;
+
+e)
+business_passenger=$(awk -F',' 'NR>1 && $3=="Business" {count++}
+END {print count}' $file)
+
+echo "Jumlah penumpang business class ada ${business_passenger} orang"
+;;
+
+*)
+echo "Soal tidak dikenali. Gunakan a, b, c, d, atau e."
+;;
+
+esac
